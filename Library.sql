a96402f9d486bf91cc4155c59e5886e257578a10
Abdihakiimi Abdi <Abdihakiim723@gmail.com>
2025-09-22 10:57:52 +0300

    modeifed the sql

--- Library.sql
+++ Library.sql
@@ -5,7 +5,6 @@ create table login(username nvarchar(50) not null,password nvarchar(50),Role nva
 create table members(memberID int IDENTITY(1,1) primary key,fullname nvarchar(50) not null,email nvarchar(50) unique not null,address nvarchar(50),JoinDate Date not null DEFAULT GETDATE())
 create table books(book_ID int IDENTITY(1,1) primary key, title nvarchar(150) not null,author nvarchar(50) not null,ISBN nvarchar(50) unique not null,publisher nvarchar(50),Yearpublished int,quantity int not null)
 CREATE TABLE BorrowedBooks (BorrowID INT IDENTITY(1,1) PRIMARY KEY,memberID INT NOT NULL,book_ID INT NOT NULL,BorrowDate DATE NOT NULL DEFAULT GETDATE(),DueDate DATE NOT NULL,ReturnDate DATE NULL,Status NVARCHAR(50) NOT NULL DEFAULT 'Borrowed',FOREIGN KEY (memberID) REFERENCES members(memberID) ON DELETE CASCADE,FOREIGN KEY (book_ID) REFERENCES books(book_ID) ON DELETE CASCADE)
-insert into login values('Abdihakiim Abdi', '12345!a', 'admin', 'abdihakiim723@gmail.com', 'kismayo' )
 Alter table books ADD AvailableCopies int not null DEFAULT 1
 Alter table books ADD TotalCopies int not null DEFAULT 1
 Alter table books ADD Genre nvarchar(20)
